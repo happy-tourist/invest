d5d5df32cd05d07002ad4bfdec9133820160e35d
Kalinin Aleksandr Aleksandrovich <AAKalinin@gksm.local>
2024-12-09 23:14:37 +0500

    feat: init

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
+import 'core-js/stable'; // Полностью включает все полифиллы
+import 'regenerator-runtime/runtime'; // Полифилл для async/await
+
 import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
